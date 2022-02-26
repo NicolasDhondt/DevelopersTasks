94b8054ac9d7865ce0f8f360de41773259677aa4
D'hondt Nicolas <54170@etu.he2b.be>
2022-02-26 15:57:00 +0100

    url to create task added

--- task/urls.py
+++ task/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import IndexView
+from . import views
 
 app_name = 'task'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('create', views.create, name='create'),
 ]
